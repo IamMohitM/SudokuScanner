b509535a27953a1f30ec4b0b15a78ab97cb2ab4a
Mohit Motwani <mohitmotwani54@gmail.com>
2020-11-30 22:04:29 +0000

    fixed bugs related to identifying and separating grid cells

--- utils.cpp
+++ utils.cpp
@@ -11,6 +11,7 @@ void drawLines(cv::Mat& houghImage, const lineType& houghLines){
     }
 }
 
+
 void printContourPoint(const contourType& contour){
     for(const auto& point: contour){
         std::cout << point << ",\n";
@@ -71,27 +72,47 @@ std::tuple<cv::Point, cv::Point, cv::Point, cv::Point> getExtremePoints(const co
     return std::make_tuple(extLeft, extRight, extTop, extBot);
 }
 
+bool contaisDigit(cv::Mat& img, float thresh = 50, bool invertImage=false){
+    float maxArea = -1;
+    cv::Mat labelImage, stats, centroids;
+
+    if(invertImage){
+        cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
+        cv::threshold(img, img, 100, 255, cv::THRESH_BINARY_INV);
+    }
+    int nlabels = cv::connectedComponentsWithStats(img, labelImage, stats, centroids, 8, CV_32S);
+    for(int label = 1; label<nlabels; ++label){
+        if(maxArea < stats.at<int>(label, cv::CC_STAT_AREA)){
+            maxArea = stats.at<int>(label, cv::CC_STAT_AREA);
+        }
+    }
+
+    return maxArea >= thresh;
+}
+
 void divideSudokuGrid(const cv::Mat& graySudokuGrid, const cv::Mat& sudokuGrid){
 //    cv::cvtColor(sudokuGrid, graySudokuGrid, cv::COLOR_BGR2GRAY);
 
     contourVector contours;
     int cellWidth = graySudokuGrid.cols / 9;
     int cellHeight = graySudokuGrid.rows / 9;
+    int nlabels;
     cv::Size resize = cv::Size(96, 96);
     cv::Mat labelImage(cv::Size(cellWidth, cellHeight), CV_32S), stats, centroids;
     double maxArea = -100;
-    cv::Mat morphed, cellImg, cellColor, extractedImg, mask(labelImage.size(), CV_8UC1, cv::Scalar(0));
+    cv::Mat morphed, cellImg, cellColor, mask(labelImage.size(), CV_8UC1, cv::Scalar(0));
     auto kernel = cv::getStructuringElement(2, cv::Size(7, 7));
     cv::Rect rect;
+    std::vector<std::tuple<int, int>> cellsWithDigits{};
 
-    for(int i=0; i < graySudokuGrid.cols; i+=cellWidth){
-        for(int j=0; j < graySudokuGrid.rows; j+=cellHeight){
-            cellImg = graySudokuGrid(cv::Rect(i, j, cellWidth, cellHeight));
-            cellColor = sudokuGrid(cv::Rect(i, j, cellWidth, cellHeight));
+    for(int i=0, row=1; i < graySudokuGrid.rows; i+=cellHeight, ++row){
+        for(int j=0, col=1; j < graySudokuGrid.cols; j+=cellWidth, ++col){
+            cellImg = graySudokuGrid(cv::Rect(j, i, cellWidth, cellHeight));
+            cellColor = sudokuGrid(cv::Rect(j, i, cellWidth, cellHeight));
 
             cv::morphologyEx(cellImg, morphed, cv::MORPH_CLOSE, kernel);
             cv::threshold(morphed, morphed, 128, 255, cv::THRESH_BINARY_INV);
-            int nlabels = cv::connectedComponentsWithStats(morphed, labelImage, stats, centroids, 8, CV_32S);
+            nlabels = cv::connectedComponentsWithStats(morphed, labelImage, stats, centroids, 8, CV_32S);
             for(int label = 1; label<nlabels; ++label){
                 if(maxArea < stats.at<int>(label, cv::CC_STAT_AREA)){
                     mask = labelImage==label;
@@ -99,18 +120,34 @@ void divideSudokuGrid(const cv::Mat& graySudokuGrid, const cv::Mat& sudokuGrid){
                 }
             }
 
+//            float avg = static_cast<float>(cv::mean(cv::mean(cellImg, mask)).val[0]);
             cv::Mat roi(cellImg.size(), cellImg.type(), cv::Scalar(0));
             roi.setTo(cv::Scalar(255), mask);
+
             cv::findContours(roi, contours, cv::noArray(), cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
             rect = cv::boundingRect(contours[0]);
-            extractedImg = cellColor(rect);
+            cv::Mat extractedImg = cellColor(rect);
 
-            cv::imshow( std::to_string(i) + ", " + std::to_string(j), extractedImg);
-            cv::moveWindow(std::to_string(i) + ", " + std::to_string(j), i+cellWidth, j+cellHeight);
+            if(extractedImg.rows > 0 && extractedImg.cols > 0) {
+                if(contaisDigit(extractedImg, 60, true)){
+                    cellsWithDigits.push_back(std::make_tuple(row, col));
+                    cv::imshow(std::to_string(row) + ", " + std::to_string(col), extractedImg);
+                    cv::moveWindow(std::to_string(row) + ", " + std::to_string(col), i + cellWidth, j + cellHeight);
+                }
+            }
+
+
             maxArea = -100;
             mask = cv::Scalar(0);
         }
     }
+//        cv::destroyAllWindows();
+
+//    std::cout << cellsWithDigits.size() << ": ";
+//    for(const auto& pair: cellsWithDigits){
+//        std::cout << "(" << std::get<0>(pair) << ", " << std::get<1>(pair) << ") ";
+//    }
+//    std::cout << '\n';
 
 }
 
